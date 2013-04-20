832286f3000f20fa2afe9d7781cd7200c1470a62
Aaron <bakeraj4@miamioh.edu>
2013-04-20 14:35:10 -0400

    Created the game over screen.
    Just the bin I/O is done.

--- src/com/voracious/graphics/GameOverScreen.java
+++ src/com/voracious/graphics/GameOverScreen.java
@@ -1,5 +1,11 @@
 package com.voracious.graphics;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import com.voracious.data.Pair;
@@ -17,5 +23,44 @@ public class GameOverScreen extends Screen {
 		//resaves the bin file
 		//shows the top 10 scores
 	}
-
+	
+	
+	public void readScores(){
+		highScores=new ArrayList<Pair<String,Integer>>(); 
+		try {
+			ObjectInputStream in =new ObjectInputStream(new FileInputStream("/scores.bin"));
+			Pair<String,Integer> obj=(Pair<String, Integer>) in.readObject();
+			while((obj=(Pair<String, Integer>) in.readObject())!=null){
+				highScores.add(obj);
+				//i am assuming that the previous scores are already sorted.
+			}
+			in.close();
+		} 
+		catch (FileNotFoundException e) {
+			//file doesn't exist so no high scores exist yet
+		} 
+		catch (IOException e) {
+			System.out.println("THERE WAS SOME ERROR IN THE FILE READING!");
+		}
+		catch (ClassNotFoundException e) {
+			System.out.println("THERE WAS NO INSTANCES OF PAIR!");
+		}
+	}
+	
+	public void writeScores(){
+		try {
+			ObjectOutputStream out=new ObjectOutputStream(new FileOutputStream("/scores.bin"));
+			for(int i=0;i<highScores.size();i++){
+				out.writeObject(highScores.get(i));
+			}
+			out.close();
+		} 
+		catch (FileNotFoundException e) {
+			//file is in the project 
+		} 
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
